a233557693247e152da7a090ac6ca09d3d02cacd
KARNASINH <karnasinh281@gmail.com>
2022-08-06 12:15:20 -0400

    Layout file
    Updated nav bar in layout file to display on all pages.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var mongoose = require("mongoose");
 var config = require("./config/globals");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+//var usersRouter = require("./routes/users");
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+//app.use("/users", usersRouter);
 
 // Configure mongoose after route declarations
 // let connStr = 'mongodb+srv://admin:strongpassword2022@cluster0.86msx.mongodb.net/Eduardo';
