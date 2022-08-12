8d45174a002eae3bb7f21d404fd3a2d4fd40d10b
KARNASINH <karnasinh281@gmail.com>
2022-08-12 19:22:16 -0400

    Images
    Updated Images on Home page

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -56,3 +56,25 @@ document.querySelectorAll(".table-sortable th").forEach((headerCell) => {
     sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
   });
 });
+
+//Javascript file to take global header and footer section on each page
+
+//Fetching data from header.html file.
+fetch("./header.html")
+  .then((response) => {
+    return response.text();
+  })
+  //Data return to the header tag of html file.
+  .then((data) => {
+    document.querySelector("header").innerHTML = data;
+  });
+
+fetch("./footer.html")
+  .then((response) => {
+    return response.text();
+  })
+
+  //Data return to the footer tag of html file.
+  .then((data) => {
+    document.querySelector("footer").innerHTML = data;
+  });
