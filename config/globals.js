10d4a539ed85b59bf39db18d33a9359d0ee2d48f
KARNASINH <karnasinh281@gmail.com>
2022-08-16 19:52:13 -0400

    Update globals.js

--- config/globals.js
+++ config/globals.js
@@ -11,8 +11,7 @@ const configurations = {
   github: {
     clientId: "bb778f386d6d3ceaba88",
     clientSecret: "56b8674aec75418a07f0ed002fe25d8cd2716730",
-    callbackUrl:
-      "http://localhost:3000/https:/knowledgecitylibrary.herokuapp.com/github/callback",
+    callbackUrl: "https:/knowledgecitylibrary.herokuapp.com/github/callback",
   },
 };
 
